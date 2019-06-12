0674ea214734850ab1ceb2440aa9b80813e02cf0
mohsen khodadadzadeh <mohsenkhodadadzadeh@mohsens-macbook-pro-2.local>
2019-06-12 21:25:36 +0430

    Initial Commit

---
+++ terminateTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  terminateTest
+//
+//  Created by mohsen khodadadzadeh on 6/12/19.
+//  Copyright © 2019 mohsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
